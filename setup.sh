2a01693f634d5b77f4eb356ffa5b6e23b92d9f62
Hale <159733534+0-404@users.noreply.github.com>
2025-12-19 22:21:52 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+apt update
+
+apt install nala -y
+
+apt install micro -y
+
+apt install apt install sway wlroots -y
+
+apt install wayland-protocols wayland-utils -y
+
+apt install xdg-desktop-portal-wlr -y
+
+apt install libdrm-amdgpu1 libdrm-intel1 libdrm-nouveau2 -y
+
+apt install libgbm1 libegl1-mesa mesa-utils mesa-vulkan-drivers -y
+
+apt install libgl1-mesa-dri -y
+
+apt install ffmpeg gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libavcodec58 libavformat58 libavutil56 -y
+
+apt install flatpak -y   #instalar flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo #adicionar repositório flathub
+
+#flatpak install flathub org.localsend.localsend_app
+
+#flatpak run org.localsend.localsend_app
+
+###################################
+### GERENCIAMENTO PÓS-INSTALAÇÃO ##
+###################################
+
+#  nmcli device status
+#  nmcli device wifi list
+#  nmcli device wifi connect SSID password SUA_SENHA
+
+
+
+
+#sudo apt 
+
+#sudo apt install micro -y
+
+#sudo apt install 
+
+#sudo apt install flatpak -y   #instalar flatpak
+
+#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo #adicionar repositório flathub
+
